cea30fff68e6f93a18df11d066e4e8047c205cdc
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-06-17 01:14:20 +0300

    Add multiple measurements

--- scripts/simulate_circle.py
+++ scripts/simulate_circle.py
@@ -16,8 +16,6 @@ class CircleTrajectory(Node):
         self._radius =2.0 # meters
         self._angular_speed = 1.0 # rad/s
         self._altitude = 3.0 # meters
-        self._x = 0.0
-        self._y = 0.0
         self._theta = 0.0 # rad
         self._dt = 0.1 # seconds
 
@@ -31,18 +29,26 @@ class CircleTrajectory(Node):
         self._poses_pub = self.create_publisher( PoseArray,'measurement/pose_array',10)
 
     def timerCallback(self):
-        self._x = self._radius + np.cos(self._theta)
-        self._y = self._radius + np.sin(self._theta)
+        self._poses_msg = PoseArray()
 
         pose_msg = Pose()
-        pose_msg.position.x = self._x
-        pose_msg.position.y = self._y
+        x = self._radius * np.cos(self._theta)
+        y = self._radius * np.sin(self._theta)
+        pose_msg.position.x = x
+        pose_msg.position.y = y
         pose_msg.position.z = self._altitude
+        self._poses_msg.poses.append(pose_msg)
+
+        x = 1.0 + self._radius * np.cos(self._theta)
+        y = 2.0 + self._radius * np.sin(self._theta)
+        pose_msg = Pose()
+        pose_msg.position.x = x
+        pose_msg.position.y = y
+        pose_msg.position.z = self._altitude
+        self._poses_msg.poses.append(pose_msg)
 
         self._theta += self._angular_speed * self._dt
 
-        self._poses_msg = PoseArray()
-        self._poses_msg.poses.append(pose_msg)
         self._poses_msg.header.frame_id = self._frame_id
         self._poses_msg.header.stamp = self.get_clock().now().to_msg()
         self._poses_pub.publish(self._poses_msg)
